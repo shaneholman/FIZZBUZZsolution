6e915db0e37e13ed8773ce79690dca19cde56c70
Shane Holman <Sholman@tql.com>
2024-06-13 14:07:20 -0400

    Add project files.

---
+++ FIZZBALLsolution/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FIZZBALLsolution;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+
+            for (var nbr = 1; nbr <= 30; nbr++)
+            {
+                if (nbr % 3 == 0 && nbr % 5 == 0)
+                {
+                    System.Console.WriteLine($"{nbr}:FIZZBUZZ");
+                }
+                else if (nbr % 3 == 0 && nbr % 5 != 0)
+                {
+                    System.Console.WriteLine($"{nbr}:FIZZ ");
+                }
+
+                else if (nbr % 3 != 0 && nbr % 5 == 0)
+                {
+                    System.Console.WriteLine($"{nbr}:BUZZ ");
+                }
+
+                else
+                    Console.WriteLine($"{nbr}");
+
+            }
+
+        }
+    }
+
